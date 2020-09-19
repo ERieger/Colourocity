afca8f1b323588c35a0f0fe04bb787a6527793ab
groovydingo <groovydingo@yahoo.com>
2020-09-19 18:04:01 +0930

    Added gravity to player

--- src/Player.js
+++ src/Player.js
@@ -5,13 +5,21 @@ class Player {
         this.w = w;
         this.h = h;
         this.r = r;
+        this.speedY = 0;
     }
 
     gravity() {
+        this.speedY++;
+        this.y += this.speedY;
 
+        if (this.y + this.h > floorHeight) {
+            this.speedY = 0;
+            this.y = floorHeight - this.h;
+        }
     }
 
     draw() {
+        fill('pink');
         rect(this.x, this.y, this.w, this.h, this.r);
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /// <reference path="./modules/p5.d.ts" />
 
 let player = new Player(0, 0, 32, 37, 5);
+let floorHeight = 520;
 
 function setup() {
     createCanvas(680, 560);
@@ -8,6 +9,9 @@ function setup() {
 
 function draw() {
     background(220);
+    noStroke();
+    fill(0);
+    rect(0, floorHeight, windowWidth, windowHeight - floorHeight);
 
     player.gravity();
     player.draw();
