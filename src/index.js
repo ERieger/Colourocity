c3b784eec9755eea9b8afa300a6cfff85dac83a1
Elijah Rieger <erieger@concordia.sa.edu.au>
2020-10-15 16:25:23 +1030

    Level storage and display function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 let player;
 let platforms = [];
+let levels = [{
+	stucture: [{
+		x: 320,
+		y: 350,
+		w: 300,
+		h: 25,
+		colour: 0
+	},
+	{
+		x: 67,
+		y: 170,
+		w: 75,
+		h: 25,
+		colour: 0
+	},
+	{
+		x: 613,
+		y: 170,
+		w: 75,
+		h: 25,
+		colour: 0
+	}],
+	keys: [],
+	colours: []
+}
+];
 
 function setup() {
 	createCanvas(680, 560);
@@ -8,14 +34,16 @@ function setup() {
 	player = new Player(width / 2, height - 65, 50, 50, color('aqua'), 7.5);
 
 	// Floor
-	createPlatform(width / 2, height - 20, width, 40, color(0));
+	createPlatform(width / 2, height - 15, width, 30, color(0));
+
+	// Roof
+	createPlatform(width / 2, 5, width, 10, color(255));
 
 	// Walls
-	createPlatform(20, height / 2, 40, height, color(0));
-	createPlatform(width - 20, height / 2, 40, height, color(0));
+	createPlatform(15, height / 2, 30, height, color(0));
+	createPlatform(width - 15, height / 2, 30, height, color(0));
 
-	// Platform
-	createPlatform(50, 350, 300, 25, color(0));
+	displayLevel(0);
 }
 
 function draw() {
@@ -29,4 +57,10 @@ function draw() {
 function createPlatform(x, y, w, h, colour) {
 	const platform = new Platform(x, y, w, h, colour);
 	platforms.push(platform);
+}
+
+function displayLevel(level) {
+	for (let i = 0; i < levels[level].stucture.length; i++) {
+		createPlatform(levels[level].stucture[i].x, levels[level].stucture[i].y, levels[level].stucture[i].w, levels[level].stucture[i].h, levels[level].stucture[i].colour);
+	}
 }
